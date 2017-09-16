8364a89db404ecb087dc3a3bd3c8f1aea378ef4c
Matthias Subileau <subileaumatthias@gmail.com>
2017-09-16 15:19:59 +0200

    add addStartIndex var diaporama.add() works fine !

--- js/script.js
+++ js/script.js
@@ -2,7 +2,9 @@ var diaporama = {
     texte: [],
     photosUrl: [],
     active: 1,
-    
+    init: true,
+    addStartIndex: 0,
+
     previous: function () {
         
     },
@@ -14,24 +16,68 @@ var diaporama = {
             alert('Le nombre de descriptions ne correspond pas au nombre de photos');
         }
 
-        var main = document.querySelector('main')
-        main.innerHTML = '';
-        for (var i = 0; i < descriptions.length; i++) {
-            var tag1 = document.createElement('figure');
-            main.appendChild(tag1);
+        this.texte = this.texte.concat(descriptions);
+        this.photosUrl = this.photosUrl.concat(urls);
+        console.log(this.texte);
+        console.log(this.photosUrl);
+
+        var main = document.querySelector('main');
+
+        if (this.init == true) {
+            for (var i = 0; i < descriptions.length; i++) {
+                var figure = document.createElement('figure');
+                main.appendChild(figure);
+                figure = document.getElementsByTagName('figure');
+                var figcaption = document.createElement('figcaption');
+                figure[i].appendChild(figcaption);
+                var img = document.createElement('img');
+                figure[i].appendChild(img);
+            }
+            this.init = false;
+            this.addStartIndex += descriptions.length;
+        } else {
+            for (var i = 0; i < descriptions.length; i++) {
+                var figure = document.createElement('figure');
+                main.appendChild(figure);
+            }
+            for (var i = this.addStartIndex; i < this.addStartIndex + descriptions.length; i++) {
+                console.log('addStartIndex = ' + this.addStartIndex);
+                console.log('i = ' + i);
+                figure = document.getElementsByTagName('figure');
+                var figcaption = document.createElement('figcaption');
+                figure[i].appendChild(figcaption);
+                var img = document.createElement('img');
+                figure[i].appendChild(img);
+            }
+            this.addStartIndex += descriptions.length;
         }
-        var figure = document.querySelectorAll('figure');
-        for (var i = 0; i < descriptions.length; i++) {
-            a = figure[i];
+
+        // jusqu'ici tout fonctionne
+
+/*        for (var i = this.texte.length; i < this.texte.length + descriptions.length; i++) {
+            console.log(i);
+        }*/
+
+/*        var figure = document.querySelectorAll('figure');
+        console.log('figure.length: ' + figure.length);
+        for (var i = this.texte.length; i < this.texte.length + descriptions.length; i++) {
+            console.log('i ()= ' +i);
+
+            g = figure[i];
+            console.log('figure[i]= ' +g);
+
             var tag2 = document.createElement('img');
             a.appendChild(tag2);
             var img = document.querySelectorAll('img');
-            img[i].setAttribute('src', urls[i]);
+            img[i].setAttribute('src', this.photosUrl[i]);
             var tag3 = document.createElement('figcaption');
             a.appendChild(tag3);
-        }
-        this.texte.push(descriptions);
-        this.photosUrl.push(urls);
+        }*/
+
+/*        for (var i = 0; i < this.texte.length; i++) {
+            document.getElementsByTagName('figcaption')[i].innerHTML = this.texte[i];
+        }*/
+
     },
 delete: function (indexDiapo) {
 
